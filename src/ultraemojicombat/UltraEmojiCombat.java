1412f0cc9f794960c4721dfbff5706d259d7cf89
Glaucio Goncalves <glaucio.coelho13@gmail.com>
2022-05-04 14:13:20 -0300

    Vetores com POO

---
+++ src/ultraemojicombat/UltraEmojiCombat.java
@@ -0,0 +1,13 @@
+package ultraemojicombat;
+
+public class UltraEmojiCombat {
+
+    public static void main(String[] args) {
+        Lutador l[] = new Lutador[6];
+        l[0] = new Lutador("Gláucio", "Brasil", 26, 1.77f, 71.0f, 11, 2, 1);
+        
+        //l.apresentar();
+        l[0].status();
+    }
+
+}
