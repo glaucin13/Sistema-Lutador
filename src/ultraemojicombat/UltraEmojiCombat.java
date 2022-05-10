31def42f0acb193ece67482064b86962999e6a55
Glaucio Goncalves <glaucio.coelho13@gmail.com>
2022-05-09 23:10:19 -0300

    Agregação entre objetos

--- src/ultraemojicombat/UltraEmojiCombat.java
+++ src/ultraemojicombat/UltraEmojiCombat.java
@@ -6,8 +6,11 @@ public class UltraEmojiCombat {
         Lutador l[] = new Lutador[6];
         l[0] = new Lutador("Gláucio", "Brasil", 26, 1.77f, 71.0f, 11, 2, 1);
         
-        //l.apresentar();
-        l[0].status();
+        l[1] = new Lutador ("Alan", "Argentina", 23, 1.55f, 72.0f, 10, 3, 2);
+        
+        Luta UEC01 = new Luta();
+        UEC01.marcarLutar(l[0], l[1]);
+        UEC01.lutar();
     }
 
 }
